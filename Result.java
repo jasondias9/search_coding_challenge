20f9fbf413707ceef475731633b5d87e62e009e2
Jason Dias <dias.jason9@gmail.com>
2016-12-04 15:15:14 -0500

    Added Result and Search skeletons

---
+++ Result.java
@@ -0,0 +1,19 @@
+public class Result {
+
+  private int index;
+  private String type;
+
+  public Result(int index, String type) {
+    this.index = index;
+    this.type = type;
+  }
+
+  public int getIndex() {
+    return this.index;
+  }
+
+  public String getType() {
+    return this.type;
+  }
+
+}
---
+++ Search.java
@@ -0,0 +1,21 @@
+public class Search {
+
+  private static int[] items;
+  private static int n_items;
+  private static int ascending;
+  private static int key;
+  private static String type;
+
+  public Search(int[] items, int n_items, int ascending, int key, String type) {
+      this.items = items;
+      this.n_items = n_items;
+      this.ascending = ascending;
+      this.key = key;
+      this.type = type;
+  }
+
+  public static Result find() {
+
+  }
+
+}
