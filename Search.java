490c4aa645d8f512cc8f08520f9e24eb02aed824
Jason Dias <dias.jason9@gmail.com>
2016-12-04 17:13:00 -0500

    Added searchLessThan and added test

--- Search.java
+++ Search.java
@@ -28,4 +28,31 @@ public class Search {
       this.type = type;
   }
 
+  public static Result search() {
+
+      switch(type) {
+        default :
+          return searchLessThan();
+
+      }
+
+  }
+
+  public static Result searchLessThan() {
+      if(n_items == 1 && items[0] < key) {
+        return new Result(0, SearchResult.FOUND_LESS);
+      }
+      for(int i = 0; i < n_items; i++) {
+        if(items[i] < key && ascending == 0) {
+          return new Result(i, SearchResult.FOUND_LESS);
+        } else if(items[i] >= key && ascending == 1) {
+          if(i-1 > 0) {
+            return new Result(i-1, SearchResult.FOUND_LESS);
+          }
+          return new Result(-1, SearchResult.NOT_FOUND);
+        }
+      }
+      return new Result(-1, SearchResult.NOT_FOUND);
+  }
+
 }
